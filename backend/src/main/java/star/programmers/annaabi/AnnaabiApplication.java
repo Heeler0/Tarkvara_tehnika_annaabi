e1a4144059312e09baef2a1708c297631304dbce
hugo.volk.2@gmail.com <NoneNone22>
2018-03-03 15:55:10 +0200

    Remove duplicate package

--- backend/src/main/java/star/programmers/annaabi/AnnaabiApplication.java
+++ backend/src/main/java/star/programmers/annaabi/AnnaabiApplication.java
@@ -1,4 +1,4 @@
-package star.programmers.annaabi.annaabi;
+package star.programmers.annaabi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/star/programmers/annaabi/AnnaabiApplicationTests.java
+++ backend/src/test/java/star/programmers/annaabi/AnnaabiApplicationTests.java
@@ -1,4 +1,4 @@
-package star.programmers.annaabi.annaabi;
+package star.programmers.annaabi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
