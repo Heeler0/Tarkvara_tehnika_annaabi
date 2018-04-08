eebb4fd54e1946491c3767082b6bacf1842794d3
huvolk <rndrgp@outlook.com>
2018-04-08 23:29:08 +0300

    Added voting system, where users can vote +1 or -1 on files.

---
+++ backend/src/main/java/star/programmers/annaabi/controllers/VoteController.java
@@ -0,0 +1,54 @@
+package star.programmers.annaabi.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import star.programmers.annaabi.database.Category;
+import star.programmers.annaabi.database.CategoryRepository;
+import star.programmers.annaabi.database.Vote;
+import star.programmers.annaabi.database.VoteRepository;
+
+import java.util.List;
+
+@RestController
+public class VoteController
+{
+    @Autowired
+    VoteRepository voteRepository;
+
+    @CrossOrigin
+    @RequestMapping("/api/vote")
+    public String vote(@RequestParam(value = "fileId") Long fileId, @RequestParam(value = "score") Integer score)
+    {
+        if (!isValidScore(score))
+            return "Invalid score.";
+
+        List<Vote> existingVoteList = voteRepository.findByFileIdAndAccountId(fileId, 58L);
+
+        if (existingVoteList.size() == 1)
+        {
+            Vote existingVote = existingVoteList.get(0);
+            existingVote.setScore(score);
+            voteRepository.save(existingVote);
+            return "Vote updated!";
+        }
+
+        // save new vote to database
+        Vote vote = new Vote();
+        vote.setAccountId(58L);
+        vote.setFileId(fileId);
+        vote.setScore(score);
+        voteRepository.save(vote);
+
+        return "Vote successful!";
+    }
+
+    public boolean isValidScore(Integer score)
+    {
+        return score == -1 || score == 0 || score == 1;
+    }
+}
