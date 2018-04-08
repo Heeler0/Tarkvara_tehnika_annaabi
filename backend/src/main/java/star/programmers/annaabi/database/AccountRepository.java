9cac03e33bceba9b155f1169020e8fe1bdba7093
huvolk <rndrgp@outlook.com>
2018-04-08 23:26:18 +0300

    Added user creation and login.

---
+++ backend/src/main/java/star/programmers/annaabi/database/AccountRepository.java
@@ -0,0 +1,10 @@
+package star.programmers.annaabi.database;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface AccountRepository extends PagingAndSortingRepository<Account, Long>
+{
+    List<Account> findByNameAndPassword(String name, String password);
+}
